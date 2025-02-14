c750eea9728137d17f13db6a83bd0ab052a4df26
Krasimir Petranov <kimim0548@gmail.com>
2025-02-14 01:59:42 +0000

    add details page

--- src/controllers/device-controller.js
+++ src/controllers/device-controller.js
@@ -3,38 +3,57 @@ import { isAuth } from "../middlewares/authMiddleware.js";
 import deviceService from "../services/deviceService.js";
 import { getErrorMessage } from "../utils/errorUtils.js";
 
-
 const deviceController = Router();
 
-deviceController.get('/catalog', async (req, res) => {
-    const devices = await deviceService.getAll();
-    res.render('devices/catalog', { devices });
+deviceController.get("/catalog", async (req, res) => {
+  const devices = await deviceService.getAll();
+  res.render("devices/catalog", { devices });
 });
 
-deviceController.get('/create', (req,res) => {
-    res.render('devices/create');
+deviceController.get("/create", (req, res) => {
+  res.render("devices/create");
 });
 
-deviceController.post('/create', isAuth, async (req,res) => {
-    const deviceData = req.body;
+deviceController.post("/create", isAuth, async (req, res) => {
+  const deviceData = req.body;
+  const userId = req.user.id;
+
+  try {
+    await deviceService.createDevice(deviceData, userId);
+
+    res.redirect("/devices/catalog");
+  } catch (err) {
+    res.render("devices/create", {
+      error: getErrorMessage(err),
+      device: deviceData,
+    });
+  }
+});
+
+deviceController.get("/:deviceId/details", async (req, res) => {
+  const deviceId = req.params.deviceId;
+  
+  const device = await deviceService.getDeviceById(deviceId);
+
+  const isOwner = device.owner.equals(req.user?.id);
+
+  const isPreferred = device.prefferedList.includes(req.user?.id);
+
+  res.render("devices/details", { device, isOwner, isPreferred });
+});
+
+deviceController.get('/:deviceId/prefer', isAuth, async (req, res) => {
+    const deviceId = req.params.deviceId;
     const userId = req.user.id;
 
     try {
-        await deviceService.createDevice(deviceData, userId);
-
-        res.redirect('/devices/catalog')
+        await deviceService.prefer(deviceId, userId);
+        res.redirect(`/devices/${deviceId}/details`)
     } catch (err) {
-        res.render('devices/create', {
+        res.render(`devices/${deviceId}/details`, {
             error: getErrorMessage(err),
-            device: deviceData
         });
-        
     }
-deviceController.get('/:deviceId/details', (req, res) => {
-    const deviceId = req.params.deviceId;
-
-    res.render('devices/details')
-});
 });
 
 export default deviceController;
--- src/services/deviceService.js
+++ src/services/deviceService.js
@@ -1,15 +1,35 @@
 import Device from "../models/Device.js";
 
+const getDeviceById = (deviceId) => Device.findById(deviceId);
+
 const getAll = () => Device.find({});
 
 const getLatest = () => Device.find({}).sort({_id: 'desc'}).limit(3);
 
 const createDevice = (deviceData, userId) => Device.create( { ...deviceData, owner: userId });
 
+const prefer = async (deviceId, userId) => {
+        const device =  await Device.findById(deviceId);
+        
+        if (device.owner.equals(userId)) {
+            throw new Error("Cannot prefer own offer!");
+        }
+
+        if (device.prefferedList.includes(userId)) {
+            throw new Error("You already preferred this offer!");
+        }
+
+        device.prefferedList.push(userId);
+
+        return device.save();
+};
+
 const deviceService = {
     createDevice,
     getLatest,
-    getAll
+    getAll,
+    getDeviceById,
+    prefer
 }
 
 export default deviceService;
