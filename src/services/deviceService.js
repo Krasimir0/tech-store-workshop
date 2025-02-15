df951d717518848cbd6654bcc53d13c5201b807c
Krasimir Petranov <kimim0548@gmail.com>
2025-02-15 00:03:30 +0000

    Add methods for update and delete devices

--- src/services/deviceService.js
+++ src/services/deviceService.js
@@ -2,11 +2,29 @@ import Device from "../models/Device.js";
 
 const getDeviceById = (deviceId) => Device.findById(deviceId);
 
+const deleteDevice = async (deviceId, userId) =>  {
+        const device = await getDeviceById(deviceId);
+        if (!device.owner.equals(userId)) {
+            throw new Error("Only owner can delete this offer");
+        }
+        return Device.findByIdAndDelete(deviceId);
+    };
+
 const getAll = () => Device.find({});
 
 const getLatest = () => Device.find({}).sort({_id: 'desc'}).limit(3);
 
 const createDevice = (deviceData, userId) => Device.create( { ...deviceData, owner: userId });
+
+const update = async (deviceId, userId, deviceData) => {
+  const device = await getDeviceById(deviceId);
+
+  if (!device.owner.equals(userId)) {
+    throw new Error("Only owner can edit this offer");
+  }
+
+  return await Device.findByIdAndUpdate(deviceId, deviceData, { runValidators: true });
+};
 
 const prefer = async (deviceId, userId) => {
         const device =  await Device.findById(deviceId);
@@ -29,7 +47,9 @@ const deviceService = {
     getLatest,
     getAll,
     getDeviceById,
-    prefer
+    prefer,
+    deleteDevice,
+    update,
 }
 
 export default deviceService;
